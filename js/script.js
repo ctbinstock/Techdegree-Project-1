093d7c37f29f1e78f3f91ee01b299dfdaa8c9aff
Chaim Tzvi Binstock <chaimtzvibinstock@gmail.com>
2018-06-19 17:43:38 +0300

    Created Function getRandomQuote

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+var randomQuote = " ";
+var randomNumber;
+
+//function to printQuote
+function printQuote() {
+
+}
+
+//function to select and return random quote object from quotes array.
+function getRandomQuote( test ) {
+  var randomNumber = Math.floor(Math.random() * quotes.length);
+  var randomQuote = quotes[randomNumber].quote;
+  return randomQuote;
+  }
